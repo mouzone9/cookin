a4c42881b382388090f2415ca1963ec9a05e0ec5
Piero <piero.neri@hotmail.fr>
2022-03-23 21:46:11 +0100

    Create custom post type banier (plugin)

---
+++ app/wp-content/plugins/promo-plugin/promo-plugin.php
@@ -0,0 +1,39 @@
+<?php 
+
+/**
+Plugin Name: Promo Plugin
+Description: Plugin de bannière de promo au contenu et fond personnalisable
+Version: 1.0.0
+Author: Team WIK
+ */
+require_once plugin_dir_path(__FILE__) . 'includes/promo-functions.php';
+
+if (!defined('ABSPATH')){
+    wp_die( 'Accès interdit' );
+}
+register_activation_hook( __FILE__, function () {
+    $admin = get_role('administrator');
+    $admin->add_cap('manage_events');
+
+    add_role( 'event_manager', 'Event Manager', [
+        'read' => true,
+        'manage_events' => true
+    ]);
+});
+register_deactivation_hook( __FILE__, function () {
+    $admin = get_role( 'administrator' );
+    $admin->remove_cap('manage_events');
+    remove_role( 'event_manager' );
+});
+
+add_filter('use_block_editor_for_post_type', 'prefix_disable_gutenberg', 10, 2);
+function prefix_disable_gutenberg($current_status, $post_type)
+{
+    // Use your post type key instead of 'product'
+    if ($post_type === 'baniers') return false;
+    return $current_status;
+}
+
+add_action( 'init', 'create_banier_posttype' );
+
+?>
--- app/wp-content/themes/wik-theme/functions.php
+++ app/wp-content/themes/wik-theme/functions.php
@@ -7,10 +7,19 @@ function wik_theme_supports() {
 	add_theme_support( 'menus' );
 }
 add_action( 'after_setup_theme', "wik_theme_supports");
+add_action( 'pre_get_posts', 'add_my_post_types_to_query' );
 
 /* ADD STYLES */
 add_action( 'wp_enqueue_scripts', function () {
 	wp_enqueue_style( 'wik-css', get_template_directory_uri() . "/dist/main.css" );
 	wp_enqueue_script( 'wik-js', get_template_directory_uri() . "/dist/main.js" );
 } );
+
+
+ 
+function add_my_post_types_to_query( $query ) {
+    if ( is_home() && $query->is_main_query() )
+        $query->set( 'post_type', array( 'post', 'baniers' ) );
+    return $query;
+}
 
