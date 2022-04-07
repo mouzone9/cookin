cbcdc936b9c267d735d2e0b27a247cd2861b5452
timothee-durand <timothee.durand70@gmail.com>
2022-04-07 12:38:23 +0200

    fix styles

--- app/wp-content/themes/wik-theme/index.php
+++ app/wp-content/themes/wik-theme/index.php
@@ -6,8 +6,6 @@ global $wp_query;
 
 
 <?php get_header(); ?>
-<h2>Femme, tu es au bon endroit pour apprendre.</h2>
-
 <div class="container">
     <h2>
         <h2><?php the_title(); ?>
@@ -17,15 +15,10 @@ global $wp_query;
 <?php
 get_search_form();
 ?>
-<?php if ( have_posts() ) : ?>
-	<?php while ( have_posts() ) : the_post(); ?>
-        <p><?php the_content(); ?></p>
-	<?php endwhile; ?>
-<?php endif; ?>
 <?php if ($the_query->have_posts()) : ?>
     <div class="card-container">
     <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
-        
+
             <div class="card">
                 <img class="img-responsive" src="<?php the_post_thumbnail_url(); ?>" alt="">
                 <div class="overlay">
@@ -34,7 +27,7 @@ get_search_form();
                     <a class="info" href="<?php the_permalink(); ?>">Voir plus</a>
                 </div>
             </div>
-        
+
     <?php endwhile; ?>
     </div>
 <?php endif; ?>
--- app/wp-content/themes/wik-theme/single-recipe.php
+++ app/wp-content/themes/wik-theme/single-recipe.php
@@ -3,18 +3,25 @@
 <?php if ( have_posts() ) : ?>
 
 	<?php while ( have_posts() ) : the_post(); ?>
-        <img src="<?php the_post_thumbnail_url(); ?>" alt="Image">
-        <h5><?php the_title(); ?></h5>
-        <p><strong>Ingrédients :</strong></p>
-        <p><?= get_post_meta( get_the_ID(), 'wik_ingredient' ) ? get_post_meta( get_the_ID(), 'wik_ingredient' )[0] : "" ?></p>
-        <p><strong>Prix :</strong>
-			<?= get_post_meta( get_the_ID(), 'wik_price' ) ? get_post_meta( get_the_ID(), 'wik_price' )[0] . "€" : "Non indiqué" ?>
-        </p>
-        <p><strong>Note :</strong>
-			<?= get_post_meta( get_the_ID(), 'wik_note' ) ? get_post_meta( get_the_ID(), 'wik_note' )[0] : "Aucune note" ?>
-        </p>
-        <p><?php the_content(); ?></p>
-        <?php comments_open() || get_comments_number() ? comments_template() : "" ?>
+        <img src="<?php the_post_thumbnail_url(); ?>" alt="Image" class="thumbnail">
+        <h2><?php the_title(); ?></h2>
+        <div class="content">
+            <div class="meta">
+                <p><strong>Ingrédients :</strong></p>
+                <p><?= get_post_meta( get_the_ID(), 'wik_ingredient' ) ? get_post_meta( get_the_ID(), 'wik_ingredient' )[0] : "" ?></p>
+                <p><strong>Prix :</strong>
+					<?= get_post_meta( get_the_ID(), 'wik_price' ) ? get_post_meta( get_the_ID(), 'wik_price' )[0] . "€" : "Non indiqué" ?>
+                </p>
+            </div>
+            <div class="recipe-recipe">
+
+                <p><?php the_content(); ?></p>
+            </div>
+        </div>
+        <div class="comments">
+	        <?php comments_open() || get_comments_number() ? comments_template() : "" ?>
+        </div>
+
 	<?php endwhile; ?>
 <?php endif; ?>
 
