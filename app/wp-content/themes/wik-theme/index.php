8624124c22deb3cc0d0b47e1ca907e0d8e075477
Piero <piero.neri@hotmail.fr>
2022-04-07 13:29:45 +0200

    Auto stash before merge of "develop" and "origin/develop"

--- app/wp-content/themes/wik-theme/index.php
+++ app/wp-content/themes/wik-theme/index.php
@@ -6,22 +6,13 @@ global $wp_query;
 
 
 <?php get_header(); ?>
-<h2>Femme, tu es au bon endroit pour apprendre.</h2>
 
-<div class="container">
-    <h2>
-        <h2><?php the_title(); ?>
-    </h2>
-    </h2>
-</div>
+
+
 <?php
 get_search_form();
 ?>
-<?php if ( have_posts() ) : ?>
-	<?php while ( have_posts() ) : the_post(); ?>
-        <p><?php the_content(); ?></p>
-	<?php endwhile; ?>
-<?php endif; ?>
+
 <?php if ($the_query->have_posts()) : ?>
     <div class="card-container">
     <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
@@ -30,7 +21,7 @@ get_search_form();
                 <img class="img-responsive" src="<?php the_post_thumbnail_url(); ?>" alt="">
                 <div class="overlay">
                     <h2><?php the_title(); ?></h2>
-                    <p><?php the_excerpt(); ?></p>
+                    
                     <a class="info" href="<?php the_permalink(); ?>">Voir plus</a>
                 </div>
             </div>
