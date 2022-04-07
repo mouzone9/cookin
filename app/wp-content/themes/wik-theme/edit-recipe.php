b9b00c03b22f35b3088a124f3e12a0f288998d74
timothee-durand <timothee.durand70@gmail.com>
2022-04-07 10:25:02 +0200

    bug fixes

--- app/wp-content/themes/wik-theme/edit-recipe.php
+++ app/wp-content/themes/wik-theme/edit-recipe.php
@@ -30,18 +30,18 @@ if (!is_user_logged_in()) {
         </div>
         <div class="input-group">
             <label for="recipe_update_recipe">Recette</label>
-            <textarea name="recipe_update_recipe" id="recipe_recipe" class="input" cols="400" rows="10" value="<?= the_content() ?>"></textarea>
+            <textarea name="recipe_update_recipe" id="recipe_recipe" class="input" cols="400" rows="10" value="<?php the_content() ?>"></textarea>
         </div>
         <div class="input-group">
             <label for="recipe_thumb">Image : </label>
-            <input type="file" name="recipe_thumb" id="recipe_thumb" multiple accept="image/*" required />
+            <input type="file" name="recipe_thumb" id="recipe_thumb" multiple accept="image/*" />
         </div>
         <input type="hidden" name="action" value="wik_update_recipe">
-        <input type="hidden" name="id" value="<?php $_GET['id'] ?>">
+        <input type="hidden" name="id" value="<?= $_GET['id'] ?>">
         <?php wp_referer_field() ?>
         <?php wp_nonce_field("recipe-update", "nonce_update_recipe") ?>
         <div class="input-group">
-            <button type="submit" name="wp-submit" id="wp-submit" class="button button-primary">Créer</button>
+            <button type="submit" name="wp-submit" id="wp-submit" class="button button-primary">Modifier</button>
         </div>
     </form>
 </div>
